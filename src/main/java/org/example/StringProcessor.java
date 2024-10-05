500efaa0bcefa1574e0a1b984450773ebea50769
EMMMABK <adilbikiev2@gmail.com>
2024-10-05 23:37:00 +0600

    add StringProcessor func isStrongPassword

--- src/main/java/org/example/StringProcessor.java
+++ src/main/java/org/example/StringProcessor.java
@@ -1,17 +1,29 @@
 package org.example;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Stack;
 
 public class StringProcessor {
     public boolean isStrongPassword(String password){
-        boolean bigLetters = false;
-        boolean smallLetters = false;
-        boolean digits = false;
-        boolean specials = false;
-        if (password == null || password.isEmpty()){
-            return false;
+        boolean strong = password.length() >= 8;
+        int upper = 0, lower = 0, digit = 0, special = 0;
+
+        for (int i=0; i<password.length(); i++){
+            char charset = password.charAt(i);
+            if(charset >= 'A' && charset <= 'Z'){
+                upper++;
+            }else if(charset >= 'a' && charset <= 'z'){
+                lower++;
+            }else if(charset >= '0' && charset <= '9'){
+                digit++;
+            }else{
+                special++;
+            }
+            if (upper > 0 && lower > 0 && digit > 0 && special > 0){
+                return strong;
+            }
         }
-        for (char c : password.toCharArray()){
-            if(Character.isUpperCase(c)) bigLetters = true;
-        }
+        return false;
     }
 
     public int calculateDigits(String sentence){
@@ -22,5 +34,7 @@ public class StringProcessor {
 
     }
 
-    public
+    public double calculateExpression(String expression){
+
+    }
 }
